fac2d8d49a459365560f7e32a569af9e221220ef
iamrosada0 <luisrosada@outlook.com>
2025-05-18 12:10:23 +0100

    feat: lambda

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// Defina a struct de acordo com o JSON esperado na mensagem SQS
+type Mensagem struct {
+	ID       string `json:"id"`
+	Conteudo string `json:"conteudo"`
+}
+
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
-	for _, message := range sqsEvent.Records {
-		fmt.Printf("Mensagem recebida: %s\n", message.Body)
+	for _, record := range sqsEvent.Records {
+		var msg Mensagem
+		err := json.Unmarshal([]byte(record.Body), &msg)
+		if err != nil {
+			log.Printf("Erro ao decodificar mensagem JSON: %v", err)
+			continue // Pula essa mensagem e continua com as outras
+		}
+
+		log.Printf("Mensagem recebida - ID: %s, Conteudo: %s", msg.ID, msg.Conteudo)
 	}
 	return nil
 }
